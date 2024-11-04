2e156c52e189828ff60846ca079c9d9a9d5899c7
NKID00 <this@nkid00.name>
2024-11-04 23:15:48 +0800

    feat: cart algo

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
 mod tree;
 
-use std::collections::HashSet;
+use tree::Class;
+use tree::*;
 
+use std::{
+    collections::{HashMap, HashSet},
+    iter::repeat_n,
+};
+
+use bimap::BiMap;
 use js_sys::{Array, Object, Reflect};
 use leptos::logging::log;
 use leptos::*;
-use leptos_dom::helpers::set_property;
+use leptos_dom::helpers::{get_property, set_property};
 use leptos_meta::*;
+use rand::{seq::IteratorRandom, SeedableRng};
+use rand_chacha::ChaCha12Rng;
 use stylers::style_str;
 use wasm_bindgen::{prelude::*, JsValue};
 use web_sys::Element;
+
+const RANDOM_SEED: u64 = 0;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -40,22 +51,29 @@ pub fn App() -> impl IntoView {
 struct DataSet {
     axises: Vec<String>,
     classes: Vec<String>,
+    axis_map: BiMap<String, Axis>,
+    class_map: BiMap<String, Class>,
+    mapped_axis: Vec<Axis>,
+    mapped_class: Vec<Class>,
     rows: Vec<(Vec<f64>, String)>,
 }
 
-fn load_data(source: String, set_data: WriteSignal<Option<DataSet>>) {
-    let csv = match source.as_str() {
+fn load_csv(source: String) -> &'static str {
+    match source.as_str() {
         "iris" => include_str!("../public/iris.csv"),
         "wine" => include_str!("../public/wine.csv"),
         "rice" => include_str!("../public/rice.csv"),
         _ => unreachable!(),
-    };
+    }
+}
+
+fn load_dataset(csv: &'static str) -> DataSet {
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(true)
         .from_reader(csv.as_bytes());
     let mut axises: Vec<String> = rdr.headers().unwrap().iter().map(str::to_owned).collect();
     axises.pop().unwrap();
-    let mut classes = HashSet::new();
+    let mut classes = HashSet::<String>::new();
     let rows = rdr
         .records()
         .map(|record| {
@@ -67,11 +85,25 @@ fn load_data(source: String, set_data: WriteSignal<Option<DataSet>>) {
             (vec.iter().map(|v| v.parse().unwrap()).collect(), last)
         })
         .collect();
-    set_data(Some(DataSet {
+    let axis_map = BiMap::from_iter(axises.iter().map(|s| s.to_owned()).zip((0..).map(Axis)));
+    let class_map = BiMap::from_iter(classes.iter().map(|s| s.to_owned()).zip((0..).map(Class)));
+    let mapped_axis = axises
+        .iter()
+        .map(|axis| *axis_map.get_by_left(axis).unwrap())
+        .collect();
+    let mapped_class = classes
+        .iter()
+        .map(|class| *class_map.get_by_left(class).unwrap())
+        .collect();
+    DataSet {
         axises,
         classes: classes.into_iter().collect(),
+        axis_map,
+        class_map,
+        mapped_axis,
+        mapped_class,
         rows,
-    }))
+    }
 }
 
 fn get(obj: &JsValue, prop: &str) -> JsValue {
@@ -94,9 +126,58 @@ extern "C" {
     fn update_chart(chart: &JsValue);
 }
 
-#[wasm_bindgen(inline_js = "export function toast_alert(alert) { alert.toast(); }")]
+#[wasm_bindgen(inline_js = "export function toast_alert_ffi(alert) { alert.toast(); }")]
 extern "C" {
-    fn toast_alert(alert: &JsValue);
+    fn toast_alert_ffi(alert: &JsValue);
+}
+
+fn toast_alert(alert: NodeRef<html::Custom>) {
+    toast_alert_ffi(&(alert.get_untracked().unwrap().into_any()));
+}
+
+fn split_train_classify_dataset(
+    dataset: DataSet,
+) -> (Vec<(DataPoint, Class)>, Vec<DataPoint>, Vec<DataPoint>) {
+    let mut rng = ChaCha12Rng::seed_from_u64(RANDOM_SEED);
+    let rows = dataset.rows;
+    let len = rows.len();
+    let mut classify_index = (0..len).choose_multiple(&mut rng, len / 5);
+    classify_index.sort();
+    (
+        rows.iter()
+            .enumerate()
+            .filter_map(|(i, row)| {
+                if classify_index.binary_search(&i).is_err() {
+                    Some((
+                        DataPoint(row.0.clone()),
+                        *dataset.class_map.get_by_left(&row.1).unwrap(),
+                    ))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+        rows.iter()
+            .enumerate()
+            .filter_map(|(i, row)| {
+                if classify_index.binary_search(&i).is_err() {
+                    Some(DataPoint(row.0.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+        rows.iter()
+            .enumerate()
+            .filter_map(|(i, row)| {
+                if classify_index.binary_search(&i).is_ok() {
+                    Some(DataPoint(row.0.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+    )
 }
 
 fn into_chart_dataset(
@@ -136,18 +217,84 @@ fn into_chart_dataset(
     obj.into()
 }
 
+fn display_tree(dataset: &DataSet, tree: &DecisionTree, indent: usize) -> String {
+    let indent_s = repeat_n(' ', indent * 2).collect::<String>();
+    match tree {
+        DecisionTree::Branch(c, left_tree, right_tree) => {
+            let left = display_tree(dataset, left_tree, indent + 1);
+            let right = display_tree(dataset, right_tree, indent + 1);
+            format!(
+                "{}if {} < {:.3} {{\n{}\n{}}} else {{\n{}\n{}}}",
+                indent_s,
+                dataset.axis_map.get_by_right(&c.axis).unwrap(),
+                c.split,
+                left,
+                indent_s,
+                right,
+                indent_s
+            )
+        }
+        DecisionTree::Leave(class) => {
+            format!(
+                "{}{}",
+                indent_s,
+                dataset.class_map.get_by_right(class).unwrap()
+            )
+        }
+    }
+}
+
+fn assess(dataset: &DataSet, result: &HashMap<Class, Vec<DataPoint>>) -> f64 {
+    let mut correct: usize = 0;
+    let mut all: usize = 0;
+    for (class, data) in result {
+        for dp in data {
+            if dataset.rows.iter().find(|row| row.0 == dp.0).unwrap().1
+                == *dataset.class_map.get_by_right(class).unwrap()
+            {
+                correct += 1;
+            }
+            all += 1;
+        }
+    }
+    correct as f64 / all as f64
+}
+
+fn log(log_ref: NodeRef<html::Custom>, line: &str) {
+    let element = log_ref.get_untracked().unwrap().into_any().clone();
+    let content = get_property(&element, "value")
+        .unwrap_or_default()
+        .as_string()
+        .unwrap();
+    set_property(&element, "value", &Some((content + line + "\n").into()));
+}
+
+macro_rules! log {
+    ($e:expr, $($t:tt)*) => (log($e, &format!($($t)*)))
+}
+
+fn timestamp() -> f64 {
+    window().performance().unwrap().now() as f64 / 1000.
+}
+
 #[component]
 pub fn Main() -> impl IntoView {
-    let (data, set_data) = create_signal(None::<DataSet>);
+    let (dataset, set_dataset) = create_signal(None::<DataSet>);
+    let (train_data, set_train_data) = create_signal(None::<Vec<(DataPoint, Class)>>);
+    let (train_classify_data, set_train_classify_data) = create_signal(None::<Vec<DataPoint>>);
+    let (classify_data, set_classify_data) = create_signal(None::<Vec<DataPoint>>);
+    let (tree, set_tree) = create_signal(None::<DecisionTree>);
+    let (result, set_result) = create_signal(None::<DataSet>);
+    let log_ref: NodeRef<html::Custom> = create_node_ref();
     let x_ref: NodeRef<html::Custom> = create_node_ref();
     let y_ref: NodeRef<html::Custom> = create_node_ref();
     create_effect(move |_| {
-        let Some(data) = data() else {
+        let Some(dataset) = dataset() else {
             return;
         };
         let x = x_ref.get_untracked().unwrap();
         let y = y_ref.get_untracked().unwrap();
-        let nodes: Vec<_> = data
+        let nodes: Vec<_> = dataset
             .axises
             .iter()
             .enumerate()
@@ -161,7 +308,7 @@ pub fn Main() -> impl IntoView {
         let element = Element::from((*x.into_any()).clone());
         element.replace_children_with_node(&Array::from_iter(nodes.clone()));
         set_property(&element, "value", &Some("".into()));
-        let nodes: Vec<_> = data
+        let nodes: Vec<_> = dataset
             .axises
             .iter()
             .enumerate()
@@ -179,39 +326,15 @@ pub fn Main() -> impl IntoView {
     let (x_axis, set_x_axis) = create_signal(None::<usize>);
     let (y_axis, set_y_axis) = create_signal(None::<usize>);
     let chart_ref: NodeRef<html::Canvas> = create_node_ref();
+    let dataset_not_found_alert: NodeRef<html::Custom> = create_node_ref();
     let x_y_same_alert: NodeRef<html::Custom> = create_node_ref();
     create_effect(move |previous_chart: Option<Option<JsValue>>| {
-        let chart = chart_ref.get_untracked()?;
-        let Some(x_axis) = x_axis() else {
-            return match previous_chart {
-                Some(Some(previous_chart)) => Some(previous_chart),
-                _ => None,
-            };
-        };
-        let Some(y_axis) = y_axis() else {
-            return match previous_chart {
-                Some(Some(previous_chart)) => Some(previous_chart),
-                _ => None,
-            };
-        };
-        if x_axis == y_axis {
-            toast_alert(&(x_y_same_alert.get_untracked().unwrap().into_any()));
-            return match previous_chart {
-                Some(Some(previous_chart)) => Some(previous_chart),
-                _ => None,
-            };
-        }
-        let data = data().unwrap();
-        let data = into_chart_dataset(x_axis, y_axis, &data.classes, &data.rows);
-        if let Some(Some(previous_chart)) = previous_chart {
-            set(&previous_chart, "data", &data);
-            update_chart(&previous_chart);
-            Some(previous_chart)
-        } else {
+        let chart = chart_ref()?;
+        if previous_chart.is_none() {
+            x_axis.track();
             let chart = (*chart.into_any()).clone();
             let config = Object::new();
             set(&config, "type", &"scatter".into());
-            set(&config, "data", &data);
             set(
                 &config,
                 "options",
@@ -242,16 +365,38 @@ pub fn Main() -> impl IntoView {
                 .into(),
             );
             let chart = new_chart(&chart, &config);
-            Some(chart)
+            return Some(chart);
+        };
+        let previous_chart = previous_chart.unwrap();
+        let Some(x_axis) = x_axis() else {
+            return previous_chart;
+        };
+        let Some(y_axis) = y_axis() else {
+            return previous_chart;
+        };
+        if x_axis == y_axis {
+            toast_alert(x_y_same_alert);
+            return previous_chart;
         }
+        let previous_chart = previous_chart.unwrap();
+        let dataset = dataset().unwrap();
+        let data = into_chart_dataset(x_axis, y_axis, &dataset.classes, &dataset.rows);
+        set(&previous_chart, "data", &data);
+        update_chart(&previous_chart);
+        Some(previous_chart)
     });
     let (class_name, style_val) = style_str! {
         main {
+            flex: 1;
             display: flex;
             flex-direction: column;
-            align-items: center;
+            width: 50%;
+            align-items: stretch;
             gap: 1rem;
-            padding: 3rem;
+            padding: 1rem;
+        }
+        h2 {
+            margin: 0;
         }
         .controls {
             display: flex;
@@ -259,20 +404,38 @@ pub fn Main() -> impl IntoView {
             align-items: end;
             gap: 1rem;
         }
-        .chart {
-            width: 50%;
+        sl-textarea::part(textarea) {
+            white-space: pre-wrap;
+            overflow-wrap: break-word;
+            font-family: "DejaVu Sans Mono", ui-monospace, "Cascadia Code", Menlo,
+            "Source Code Pro", Consolas, monospace;
         }
     };
     view! {
         class = class_name,
         <Style> { style_val } </Style>
         <main>
+            <h2> "决策树" </h2>
             <div class="controls">
                 <sl-select label="选择数据集" on:sl-change=move |ev: JsValue| {
+                    let begin = timestamp();
                     let source = get(&get(&ev, "target"), "value").as_string().unwrap();
                     set_x_axis(None);
                     set_y_axis(None);
-                    load_data(source, set_data);
+                    set_tree(None);
+                    let csv = load_csv(source);
+                    let dataset = load_dataset(csv);
+                    let (
+                        train_data,
+                        train_classify_data,
+                        classify_data
+                    ) = split_train_classify_dataset(dataset.clone());
+                    set_dataset(Some(dataset));
+                    let duration = (0.001f64).max(timestamp() - begin);
+                    log!(log_ref, "数据集加载完毕, 训练集点数 {}, 测试集点数 {}, 用时 {duration:.3} 秒", train_data.len(), classify_data.len());
+                    set_train_data(Some(train_data));
+                    set_train_classify_data(Some(train_classify_data));
+                    set_classify_data(Some(classify_data));
                 }>
                     <sl-option value="iris"> "鸢尾花的尺寸" </sl-option>
                     <sl-option value="wine"> "红酒的理化性质" </sl-option>
@@ -294,12 +457,39 @@ pub fn Main() -> impl IntoView {
                 </sl-select>
                 <sl-button-group>
                     <sl-button on:click=move |_| {
-                        log!("train");
+                        let begin = timestamp();
+                        let Some(dataset) = dataset() else {
+                            toast_alert(dataset_not_found_alert);
+                            return;
+                        };
+                        let tree = train(
+                            dataset.mapped_axis.as_slice(),
+                            dataset.mapped_class.as_slice(),
+                            train_data().unwrap(),
+                        );
+                        let result = classify(&tree, train_classify_data().unwrap());
+                        let correct_rate = assess(&dataset, &result);
+                        log!(log_ref, "决策树:\n{}", display_tree(&dataset, &tree, 0));
+                        set_tree(Some(tree));
+                        let duration = (0.001f64).max(timestamp() - begin);
+                        log!(log_ref, "训练完毕, 训练集分类正确率 {:.3}%, 用时 {duration:.3} 秒", correct_rate * 100.);
                     }> "训练" </sl-button>
                     <sl-button on:click=move |_| {
-                        log!("classify");
-                    }> "拟合" </sl-button>
+                        let begin = timestamp();
+                        let Some(dataset) = dataset() else {
+                            toast_alert(dataset_not_found_alert);
+                            return;
+                        };
+                        let result = classify(&tree().unwrap(), classify_data().unwrap());
+                        let correct_rate = assess(&dataset, &result);
+                        let duration = (0.001f64).max(timestamp() - begin);
+                        log!(log_ref, "分类完毕, 测试集分类正确率 {:.3}%, 用时 {duration:.3} 秒", correct_rate * 100.);
+                    }> "分类" </sl-button>
                 </sl-button-group>
+                <sl-alert variant="danger" duration="3000" closable ref=dataset_not_found_alert>
+                    <sl-icon slot="icon" name="exclamation-octagon"></sl-icon>
+                    "必须选择数据集"
+                </sl-alert>
                 <sl-alert variant="danger" duration="3000" closable ref=x_y_same_alert>
                     <sl-icon slot="icon" name="exclamation-octagon"></sl-icon>
                     "X 轴和 Y 轴数据不能相同"
@@ -308,6 +498,7 @@ pub fn Main() -> impl IntoView {
             <div class="chart">
                 <canvas ref=chart_ref />
             </div>
+            <sl-textarea label="日志" rows="15" ref=log_ref></sl-textarea>
         </main>
     }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,18 +1,247 @@
-#[derive(Debug, Clone)]
-struct Criteria {
-    axis: usize,
-    split: f64,
+use core::f64;
+use std::{
+    collections::HashMap,
+    ops::{Deref, Index, IndexMut},
+};
+
+const MIN_ELEMENTS_TO_SPLIT: usize = 3;
+const MAX_DEPTH: usize = 7;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Axis(pub i32);
+
+#[derive(Debug, Clone, Copy)]
+pub struct Criterion {
+    pub axis: Axis,
+    pub split: f64,
+}
+
+impl Criterion {
+    fn split(
+        &self,
+        data: &Vec<(DataPoint, Class)>,
+    ) -> (Vec<(DataPoint, Class)>, Vec<(DataPoint, Class)>) {
+        let mut left = Vec::new();
+        let mut right = Vec::new();
+        for (dp, class) in data {
+            if dp[self.axis] < self.split {
+                left.push((dp.clone(), *class));
+            } else {
+                right.push((dp.clone(), *class));
+            }
+        }
+        (left, right)
+    }
+    fn classify(&self, data: &Vec<DataPoint>) -> (Vec<DataPoint>, Vec<DataPoint>) {
+        let mut left = Vec::new();
+        let mut right = Vec::new();
+        for dp in data {
+            if dp[self.axis] < self.split {
+                left.push(dp.clone());
+            } else {
+                right.push(dp.clone());
+            }
+        }
+        (left, right)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Class(pub i32);
+
+impl Deref for Class {
+    type Target = i32;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 #[derive(Debug, Clone)]
-struct Class(String);
+pub struct DataPoint(pub Vec<f64>);
+
+impl Index<Axis> for DataPoint {
+    type Output = f64;
+
+    fn index(&self, index: Axis) -> &Self::Output {
+        &self.0[index.0 as usize]
+    }
+}
+
+impl Index<&Axis> for DataPoint {
+    type Output = f64;
+
+    fn index(&self, index: &Axis) -> &Self::Output {
+        &self.0[index.0 as usize]
+    }
+}
+
+impl IndexMut<Axis> for DataPoint {
+    fn index_mut(&mut self, index: Axis) -> &mut Self::Output {
+        &mut self.0[index.0 as usize]
+    }
+}
+
+impl IndexMut<&Axis> for DataPoint {
+    fn index_mut(&mut self, index: &Axis) -> &mut Self::Output {
+        &mut self.0[index.0 as usize]
+    }
+}
+
+impl Deref for DataPoint {
+    type Target = Vec<f64>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 #[derive(Debug, Clone)]
-enum Tree {
-    Branch(Criteria, Box<Tree>, Box<Tree>),
+pub enum DecisionTree {
+    Branch(Criterion, Box<DecisionTree>, Box<DecisionTree>),
     Leave(Class),
 }
 
-fn loss() {}
+fn gini_loss(classes: &[Class], vec: &[(DataPoint, Class)]) -> f64 {
+    let mut loss = 0f64;
+    for class in classes {
+        let p = 1f64 / (vec.len() as f64)
+            * (vec.iter().filter(|(_dp, cls)| cls == class).count() as f64);
+        loss += p * (1f64 - p);
+    }
+    loss
+}
 
-fn gradient_descent() {}
+fn find_criterion(axises: &[Axis], classes: &[Class], data: &Vec<(DataPoint, Class)>) -> Criterion {
+    let mut min_impurity = f64::MAX;
+    let mut min_impurity_axis = axises[0];
+    let mut min_impurity_split = 0f64;
+    for axis in axises {
+        let mut max = f64::MIN;
+        let mut min = f64::MAX;
+        for (dp, _class) in data {
+            max = max.max(dp[axis]);
+            min = min.min(dp[axis]);
+        }
+        let step = (max - min) / 100.;
+        if step == 0. {
+            continue;
+        }
+        let mut split_criterion = min + step;
+        while split_criterion <= max {
+            let c = Criterion {
+                axis: *axis,
+                split: split_criterion,
+            };
+            let (left, right) = c.split(data);
+            if left.is_empty() || right.is_empty() {
+                split_criterion += step;
+                continue;
+            }
+            let left_loss = gini_loss(classes, &left);
+            let right_loss = gini_loss(classes, &right);
+            let impurity = ((left.len() as f64) / (data.len() as f64)) * left_loss
+                + ((right.len() as f64) / (data.len() as f64)) * right_loss;
+            if impurity < min_impurity {
+                min_impurity = impurity;
+                min_impurity_axis = *axis;
+                min_impurity_split = split_criterion;
+            }
+            split_criterion += step;
+        }
+    }
+    Criterion {
+        axis: min_impurity_axis,
+        split: min_impurity_split,
+    }
+}
+
+#[derive(Debug, Clone)]
+enum TrainTree {
+    Branch(Criterion, Box<TrainTree>, Box<TrainTree>),
+    Leave(Vec<(DataPoint, Class)>),
+}
+
+impl From<TrainTree> for DecisionTree {
+    fn from(value: TrainTree) -> Self {
+        match value {
+            TrainTree::Branch(criterion, train_tree, train_tree1) => DecisionTree::Branch(
+                criterion,
+                Box::new(DecisionTree::from(*train_tree)),
+                Box::new(DecisionTree::from(*train_tree1)),
+            ),
+            TrainTree::Leave(vec) => {
+                let mut counter = HashMap::<Class, usize>::new();
+                for (_dp, class) in vec {
+                    match counter.get_mut(&class) {
+                        Some(c) => *c += 1,
+                        None => {
+                            counter.insert(class, 1);
+                        }
+                    }
+                }
+                DecisionTree::Leave(
+                    *counter
+                        .iter()
+                        .max_by_key(|(_class, count)| *count)
+                        .unwrap()
+                        .0,
+                )
+            }
+        }
+    }
+}
+
+fn grow(
+    axises: &[Axis],
+    classes: &[Class],
+    data: Vec<(DataPoint, Class)>,
+    depth: usize,
+) -> TrainTree {
+    if depth < MAX_DEPTH && data.len() > MIN_ELEMENTS_TO_SPLIT {
+        let c = find_criterion(axises, classes, &data);
+        let (left, right) = c.split(&data);
+        TrainTree::Branch(
+            c,
+            Box::new(grow(axises, classes, left, depth + 1)),
+            Box::new(grow(axises, classes, right, depth + 1)),
+        )
+    } else {
+        TrainTree::Leave(data)
+    }
+}
+
+pub fn train(axises: &[Axis], classes: &[Class], vec: Vec<(DataPoint, Class)>) -> DecisionTree {
+    grow(axises, classes, vec, 0).into()
+}
+
+fn merge_result(
+    mut left: HashMap<Class, Vec<DataPoint>>,
+    right: HashMap<Class, Vec<DataPoint>>,
+) -> HashMap<Class, Vec<DataPoint>> {
+    for (class, mut right_vec) in right {
+        match left.get_mut(&class) {
+            Some(left_vec) => left_vec.append(&mut right_vec),
+            None => {
+                left.insert(class, right_vec);
+            }
+        }
+    }
+    left
+}
+
+pub fn classify(tree: &DecisionTree, data: Vec<DataPoint>) -> HashMap<Class, Vec<DataPoint>> {
+    match tree {
+        DecisionTree::Branch(c, left_tree, right_tree) => {
+            let (left_data, right_data) = c.classify(&data);
+            let left = classify(left_tree, left_data);
+            let right = classify(right_tree, right_data);
+            merge_result(left, right)
+        }
+        DecisionTree::Leave(class) => {
+            let mut result = HashMap::new();
+            result.insert(*class, data);
+            result
+        }
+    }
+}
